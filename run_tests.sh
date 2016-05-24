1c76b21f9860187cb5846b135f8043185a60578d
Peter Goetz <peter.gtz@gmail.com>
2016-05-25 00:12:06 +0200

    Time test runs

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$GOPATH/bin/ginkgo pegomock/mockgen
-$GOPATH/bin/ginkgo -r --randomizeAllSpecs --randomizeSuites --race --trace
+time $GOPATH/bin/ginkgo pegomock/mockgen
+time $GOPATH/bin/ginkgo -r --randomizeAllSpecs --randomizeSuites --race --trace
