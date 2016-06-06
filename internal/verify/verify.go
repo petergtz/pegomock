06d29d46d3aa4ab637dc0784e321ab7bd463f075
Peter Goetz <peter.gtz@gmail.com>
2016-06-06 14:19:58 +0200

    Fix verify

--- internal/verify/verify.go
+++ internal/verify/verify.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func NotNil(arg interface{}, message string, a ...interface{}) {
 	if arg == nil {
-		panic(fmt.Sprintf(message, a))
+		panic(fmt.Sprintf(message, a...))
 	}
 }
 
 func Argument(arg bool, message string, a ...interface{}) {
 	if !arg {
-		panic(fmt.Sprintf(message, a))
+		panic(fmt.Sprintf(message, a...))
 	}
 }
