9980397a250351e94d4d4f61e23a44ea7019dc3c
Peter Goetz <peter.gtz@gmail.com>
2019-06-13 12:45:37 +0200

    wip

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/petergtz/pegomock
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/tools v0.0.0-20190612232758-d4e310b4a8a5
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,19 +2,21 @@
 
 set -ex
 cd $(dirname $0)/..
+export PATH=$PATH:~/go/bin
+go install github.com/onsi/ginkgo/ginkgo
 
 PACKAGES_TO_SKIP='generate_test_mocks/xtools_go_loader,generate_test_mocks/gomock_reflect,generate_test_mocks/gomock_source'
 rm -f mock_display_test.go
 rm -rf matchers
-$GOPATH/bin/ginkgo -succinct generate_test_mocks/xtools_go_loader
-$GOPATH/bin/ginkgo -r -skipPackage=$PACKAGES_TO_SKIP --randomizeAllSpecs --randomizeSuites --race --trace -cover
+ginkgo -succinct generate_test_mocks/xtools_go_loader
+ginkgo -r -skipPackage=$PACKAGES_TO_SKIP --randomizeAllSpecs --randomizeSuites --race --trace -cover
 
 rm -f mock_display_test.go
 rm -rf matchers
-$GOPATH/bin/ginkgo -succinct generate_test_mocks/gomock_reflect
-$GOPATH/bin/ginkgo --randomizeAllSpecs --randomizeSuites --race --trace -cover
+ginkgo -succinct generate_test_mocks/gomock_reflect
+ginkgo --randomizeAllSpecs --randomizeSuites --race --trace -cover
 
 rm -f mock_display_test.go
 rm -rf matchers
-$GOPATH/bin/ginkgo -succinct generate_test_mocks/gomock_source
-$GOPATH/bin/ginkgo --randomizeAllSpecs --randomizeSuites --race --trace -cover
+ginkgo -succinct generate_test_mocks/gomock_source
+ginkgo --randomizeAllSpecs --randomizeSuites --race --trace -cover
